7eb720014d7175aaec4c27196d2baa7db19c84e8
championgirl <66965981+championgirl@users.noreply.github.com>
2022-05-02 18:25:56 -0700

    TeaPartyWIP

---
+++ src/_99_extra/_04_tea_party/TeaParty.java
@@ -0,0 +1,22 @@
+package _99_extra._04_tea_party;
+
+public class TeaParty {
+
+	public String welcome(String name, boolean isWoman, boolean isKnighted) {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+return null;
+	}
+
+}
